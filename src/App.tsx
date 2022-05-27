2167c3b1dff125500da40591cb6637da1961529c
Rachel Hill <hillrachel187@gmail.com>
2022-05-27 16:28:17 -0600

    Add basic typescript structure for app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type State = {
+  breweries: string[];
+}
+
+class App extends React.Component<{}, State> {
+  state: State = {
+    breweries: [],
+  }
+
+
+
+
+  render() {
+    const { breweries } = this.state
+    return (
+      <div>
+      <h1>HI</h1>
+      <p>{breweries}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
