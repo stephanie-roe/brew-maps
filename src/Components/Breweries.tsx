3e3a37c3aa9ae35be3259f9caa666fefc047bbfa
Igor Decess <i.decess@comcast.net>
2022-05-28 11:37:32 -0600

    Add mapping over the breweries

--- src/Components/Breweries.tsx
+++ src/Components/Breweries.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import '../Styles/Breweries.css';
+import Brewery from './Brewery';
 
 type BreweriesProps = {
   newBrewery: object[]
 }
 
 const Breweries = ({ newBrewery }: BreweriesProps): JSX.Element => {
+  const result = newBrewery.map(singleBrewery => {
+    return (
+      <Brewery breweryObject={singleBrewery} key={newBrewery.indexOf(singleBrewery)} />
+    )
+  })
   return (
-    <h2>Breweries</h2>
+    <div>
+      {result}
+    </div>
   )
 }
 
---
+++ src/Components/Brewery.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Styles/Brewery.css';
+
+type BreweryProps = {
+  breweryObject: {}
+}
+
+const Brewery = ({ breweryObject }: BreweryProps): JSX.Element => {
+  return (
+
+    <h2>Breweries</h2>
+  )
+}
+
+
+export default Brewery;
