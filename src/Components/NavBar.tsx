32a8948ea8b787a022632cec48a254a92431f561
Nicholas Ao <aominhlong@yahoo.com>
2022-05-31 19:44:59 -0600

    Set up NavBar component skeleton

---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../Styles/NavBar.css';
+
+type NavBarState={
+    
+}
+
+class NavBar extends Component {
+    state: NavBarState={
+
+    }
+}
+
+export default NavBar
